a9a809f226341d083514b5d025f5c8955658680a
UNKNOWNツ <UNKNOWNV5@hotmail.com>
2026-06-04 19:04:11 +0200

    Add files via upload

---
+++ unknown/project/entry.cpp
@@ -0,0 +1,47 @@
+#include <stdafx.hpp>
+
+int main( )
+{
+	{
+		if ( !g::console.initialize( " :> " ) )
+		{
+			return 1;
+		}
+
+		g::init( );
+
+		if ( !g::input.initialize( ) )
+		{
+			return 1;
+		}
+
+		if ( !g::memory.initialize( L"cs2.exe" ) )
+		{
+			return 1;
+		}
+	}
+
+	{
+		if ( !g::modules.initialize( ) )
+		{
+			return 1;
+		}
+
+		if ( !g::offsets.initialize( ) )
+		{
+			return 1;
+		}
+	}
+
+	{
+		std::thread( threads::game ).detach( );
+		std::thread( threads::combat ).detach( );
+
+		if ( !g::render.initialize( ) )
+		{
+			return 1;
+		}
+	}
+
+	return 0;
+}
